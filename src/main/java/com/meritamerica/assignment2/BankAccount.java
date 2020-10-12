58e8df49e464bd431d5e602e99aaebc87f87c21b
Rod <rodwombles@gmail.com>
2020-10-11 22:37:19 -0500

    Changes to be committed: 	new file:   BankAccount.java

---
+++ src/main/java/com/meritamerica/assignment2/BankAccount.java
@@ -0,0 +1,44 @@
+package com.meritamerica.assignment2;
+
+public class BankAccount {
+	
+	/*
+	 * Instance variables;
+	 */
+	
+	/*
+	 * Constructors:
+	 */
+	
+	public BankAccount(double balance, double interestRate) {
+		
+	}
+	
+	public BankAccount(long accountNumber, double balance, double interestRate) {
+		
+	}
+	
+	/*
+	 * Class methods:
+	 */
+	
+	public long getAccountNumber() {
+		return 0l;
+	}
+	
+	public double getInterestRate() {
+		return 0.0;
+	}
+	
+	public boolean withdraw(double amount) {
+		return false;
+	}
+	
+	public boolean deposit(double amount) {
+		return false;
+	}
+	
+	public double furuteValue(int years) {
+		return 0.0;
+	}
+}
